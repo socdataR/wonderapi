3f3582a3e7bb7759cdd70648fbb561b8f8868b85
Elin Waring <elin.waring@gmail.com>
2017-06-30 16:51:42 -0400

    Update the basic files.

---
+++ R/natality-package.R
@@ -0,0 +1,16 @@
+#' natality package
+#' @description
+#'  It provides easy access to CDC wonder data primarily for sociological topics.
+#'
+#' @name natality-package
+#' @aliases natality
+#' @docType package
+#' @keywords package
+
+NULL
+
+
+#' @importFrom magrittr %>%
+#' @export
+
+magrittr::`%>%`
