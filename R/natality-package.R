35da696f26b2867d0839c1e628ce01c56321b70b
Elin Waring <elin.waring@gmail.com>
2017-06-30 20:59:20 -0400

    Starting some files.

--- R/natality-package.R
+++ R/natality-package.R
@@ -1,7 +1,8 @@
 #' natality package
 #' @description
-#'  It provides easy access to CDC wonder data primarily for sociological topics.
-#'
+#'  It provides easy access to CDC wonder natality data primarily for sociological topics. It leverages the wondr package's
+#'  general approach to make a more specific and human readable set of functions to build queries, retrieve data, and
+#'  prepare data for either reports or further analysis.
 #' @name natality-package
 #' @aliases natality
 #' @docType package
