a6446dee05ab9aa398c40417edaf2e97fe6dcd7a
jtr13 <jtr13@columbia.edu>
2017-07-25 09:05:59 -0400

    switched back to query_result

--- data-raw/makelookuptable.R
+++ data-raw/makelookuptable.R
@@ -1,30 +1,14 @@
 # create three lookup tables:
 # variablelookup.csv, measurelookup.csv, otherparamlookup.csv
 # used to create codebook
-
-makelookuptables <- function() {
-
-get_var_attr <- function(variablenode) {
-        code <- variablenode %>% xml_attr("code")
-        label <- variablenode %>% xml_attr("label")
-        valuenodes <- variablenode %>% xml_nodes("value")
-        if (length(valuenodes) > 0) {
-            valuecode <- valuenodes %>% xml_attr("code")
-            valuelabel <- valuenodes %>% xml_attr("label")
-        }
-        else {
-            valuecode <- NA
-            valuelabel <- NA
-        }
-        tibble(code, label, valuecode, valuelabel)
-}
-
-get_param_attr <- function(variablenode) {
-    code <- variablenode %>% xml_attr("code")
-    label <- variablenode %>% xml_attr("label")
-    valuenodes <- variablenode %>% xml_nodes("option")
+library(xml2)
+library(tidyverse)
+get_var_attr <- function(node) {
+    code <- node %>% xml_attr("code")
+    label <- node %>% xml_attr("label")
+    valuenodes <- node %>% xml_find_all("value")
     if (length(valuenodes) > 0) {
-        valuecode <- valuenodes %>% xml_attr("value")
+        valuecode <- valuenodes %>% xml_attr("code")
         valuelabel <- valuenodes %>% xml_attr("label")
     }
     else {
@@ -34,33 +18,41 @@ get_param_attr <- function(variablenode) {
     tibble(code, label, valuecode, valuelabel)
 }
 
+get_param_attr <- function(node) {
+    code <- node %>% xml_attr("code")
+    label <- node %>% xml_attr("label")
+    optnodes <- node %>% xml_nodes("option")
+    if (length(optnodes) > 0) {
+        optcode <- optnodes %>% xml_attr("value")
+        optlabel <- optnodes %>% xml_attr("label")
+    }
+    else {
+        optcode <- NA
+        optlabel <- NA
+    }
+    tibble(code, label, optcode, optlabel)
+}
 
+makelookuptables <- function(query_result) {
+    # variables (start with "D66.V")
+    variables <- query_result %>%
+        xml_find_all("//dataset/variable[@code]")
+    varlookup <- map_df(variables, get_var_attr)
+    write_csv(varlookup, "data/varlookup.csv")
 
+    measures <- query_result %>%
+        xml_find_all("//dataset/measure")
+    code <- measures %>% xml_attr("code")
+    label <- measures %>% xml_attr("label")
+    measurelookup <- tibble(code, label)
+    write_csv(measurelookup, "data/measurelookup.csv")
 
-variables <- query_result %>%
-    xml_find_all("//dataset/variable[@code]")
-
-measures <- query_result %>%
-    xml_find_all("//dataset/measure")
-
-code <- measures %>% xml_attr("code")
-label <- measures %>% xml_attr("label")
-
-parameters <- query_result %>%
-    xml_find_all("response/request/other-parameters/parameter")
-
-rbind(map_df(variables, get_var_attr),
-      tibble(code, label, valuecode = NA, valuelabel = NA),
-      map_df(parameters, get_param_attr))
-
+    parameters <- query_result %>%
+        xml_find_all("response/request/other-parameters/parameter")
+    paramlookup <- map_df(parameters, get_param_attr)
+    write_csv(paramlookup, "data/paramlookup.csv")
 }
 
 query_result <- read_xml("query_result.xml")
-lookup <- makelookuptable(query_result)
 
-params <- parameters %>% xml_attr("code")
-resultparams <- cbind(params, parameters
-                      %>% xml_nodes("value") %>% xml_text())
-
-resultparams
-#write_csv(lookup, "D66lookuptable.csv")
+makelookuptables(query_result)
