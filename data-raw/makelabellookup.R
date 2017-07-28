d97af480a18bab24f1ab72adea24181762e55714
jtr13 <jtr13@columbia.edu>
2017-07-28 17:33:14 -0400

    check for type in D76 and fix

--- data-raw/makelabellookup.R
+++ data-raw/makelabellookup.R
@@ -1,9 +1,16 @@
 # makes label lookup table
-webdata <- read_html("data/webdata.html")
+makelablelookup <- function(webdata) {
+    webform <- html_form(webdata)[[3]]
+    dbcode <- webform$fields$dataset_code$value
+
 labels <- webdata %>% html_nodes("label")
 forfield <- labels %>% html_attr("for") %>%
     str_replace_all("[$S|$C|$T|$R]", "")
 
+# fix typo in D76
+if (dbcode == "D76") {
+    forfield <- str_replace_all(forfield, "O_icd", "O_ucd")
+}
 
 labeltext <- labels %>% html_text() %>%
     # get rid of long help links
@@ -13,4 +20,5 @@ label_df <- data.frame(code = forfield, label = labeltext,
                        stringsAsFactors = FALSE) %>%
     na.omit()
 
-write_csv(label_df, "data/labellookup.csv")
+write_csv(label_df, paste0("data/", dbcode, "labellookup.csv"))
+}
