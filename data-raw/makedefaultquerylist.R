a6c13eee4ce1caa6e5d193d75fd1ef53e25f32d1
jtr13 <jtr13@columbia.edu>
2017-07-29 17:47:41 -0400

    change the way list is named

--- data-raw/makedefaultquerylist.R
+++ data-raw/makedefaultquerylist.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(rvest)
 
 
-# see wondr::add_param()
+# variation of wondr::add_param()
 make_param_list <- function(param_node) {
     name <- param_node %>% xml_node("name") %>% xml_text()
     value <- param_node %>% xml_nodes("value") %>%
@@ -11,18 +11,16 @@ make_param_list <- function(param_node) {
     pvec <- as.list(value)
     pvec <- setNames(pvec, rep("value", length(pvec)))
     pvec <- c(name = name, pvec)
-    pvec
+    list(parameter = pvec)
 }
 
 make_query_list <- function(filename) {
     dbcode <- strsplit(filename, "_")[[1]][1]
-    default_xml <- read_xml(file.path("data-raw",
+    default_xml <- read_xml(paste0("data-raw/",
                                         filename)) %>%
         xml_nodes("parameter")
-    default_list <- map(default_xml, make_param_list)
-    setNames(default_list, rep("parameter",
-                               length(default_list)))
-    filename <- paste0("data/", dbcode, "querydefaults.RData")
-    save(default_list, file = filename)
+    default_list <- sapply(default_xml, make_param_list)
+    newname <- paste0("data/", dbcode, "querydefaults.RData")
+    save(default_list, file = newname)
 }
 
