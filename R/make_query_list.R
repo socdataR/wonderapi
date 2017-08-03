948e451a5c6dedeab74a28a4b1cf196d0b8d3480
jtr13 <jtr13@columbia.edu>
2017-08-02 22:31:34 -0400

    namespacing, assign + save(list=)

--- R/make_query_list.R
+++ R/make_query_list.R
@@ -1,13 +1,10 @@
 # make default list
-library(tidyverse)
-library(rvest)
-
 
 # variation of wondr::add_param()
 make_param_list <- function(param_node) {
-    name <- param_node %>% xml_node("name") %>% xml_text()
-    value <- param_node %>% xml_nodes("value") %>%
-        xml_text()
+    name <- param_node %>% rvest::xml_node("name") %>% xml2::xml_text()
+    value <- param_node %>% rvest::xml_nodes("value") %>%
+        xml2::xml_text()
     pvec <- as.list(value)
     pvec <- setNames(pvec, rep("value", length(pvec)))
     pvec <- c(name = name, pvec)
@@ -16,10 +13,11 @@ make_param_list <- function(param_node) {
 
 make_query_list <- function(filename) {
     dbcode <- strsplit(filename, "_")[[1]][1]
-    default_xml <- read_xml(paste0("data/", filename)) %>%
-        xml_nodes("parameter")
+    default_xml <- xml2::read_xml(paste0("data-raw/", filename)) %>%
+        rvest::xml_nodes("parameter")
     default_list <- sapply(default_xml, make_param_list)
-    newname <- paste0("data/", dbcode, "querydefaults.RData")
-    save(default_list, file = newname)
+    objname <- paste0(dbcode, "querydefaults")
+    assign(objname, default_list)
+    save(list = objname, file = paste0("data/", objname, ".RData"))
 }
 
