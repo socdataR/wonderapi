0ff6473b5b94ae0a084edf1a682e2a04b3d129b9
jtr13 <jtr13@columbia.edu>
2017-08-02 22:30:27 -0400

    new data files and man entries

---
+++ data-raw/create_data_files.R
@@ -0,0 +1,22 @@
+# create all data files
+# entered manually from https://wonder.cdc.gov/
+
+library(xml2) # not finding xml2::xml_text() for some reason
+
+dbnamelookup <- data.frame(dblabel = c("Births", "Detailed Mortality"),
+                           dbname = c("natality-current", "ucd-icd10"),
+                           dbcode = c("D66", "D76"),
+                           dbagree = c("action-I Agree", "action-I Agree"),
+                           stringsAsFactors = FALSE)
+
+save(dbnamelookup, file = "data/dbnamelookup.Rdata")
+
+# add label lookups
+db_to_add <- c("D66", "D76")
+purrr::map(db_to_add, make_label_lookup)
+
+# add query default parameters
+qd_to_add <- c("D66_BirthsOnly-req.xml", "D76_Example1-req.xml")
+purrr::map(qd_to_add, make_query_list)
+
+
