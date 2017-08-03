ca4f349065320cbbd5106e46f0ce905a5567e27d
jtr13 <jtr13@columbia.edu>
2017-08-03 13:41:02 -0400

    Rdata consistency

--- R/create_ignore_file.R
+++ R/create_ignore_file.R
@@ -3,4 +3,4 @@
 ignore <- c("O_location", "F_D66.V21", "F_D66.V22",
             "F_D66.V37", "V_D66.V21", "V_D66.V22",
             "V_D66.V37")
-save(ignore, file = "data/D66ignore.RData")
+save(ignore, file = "data/D66ignore.Rdata")
