ab971bbc628dcf8282e7285503e58b9c48c7a122
jtr13 <jtr13@columbia.edu>
2017-08-09 13:52:24 -0400

    moved dbnamelookup to .csv file

--- data-raw/create_data_files.R
+++ data-raw/create_data_files.R
@@ -1,25 +1,39 @@
-# create all data files
-# entered manually from https://wonder.cdc.gov/
+# create data files and vignette:
+# 1. read dbnamelookup (database name lookup)
+# 2. label lookup lists
+# 3. query default lookup lists
+# 4. codebook vignettes
 
-library(xml2) # not finding xml2::xml_text() for some reason
+library(dplyr)
+library(readr)
+library(wonderapi)
 
-dbnamelookup <- data.frame(dblabel = c("Births", "Detailed Mortality"),
-                           dbname = c("natality-current", "ucd-icd10"),
-                           dbcode = c("D66", "D76"),
-                           dbagree = c("action-I Agree", "action-I Agree"),
-                           stringsAsFactors = FALSE)
+# read file of data entered manually from https://wonder.cdc.gov/
+dbnamelookup <- read_csv("data-raw/dbnamelookup.csv")
 
-# add label lookups
-db_to_add <- c("D66", "D76")
-label_list <- purrr::map(db_to_add, make_label_lookup) %>%
-    setNames(paste0(db_to_add, "labellookup"))
+# since package needs dbnamelookup, do the following before continuing:
+devtools::use_data(dbnamelookup, internal = TRUE, overwrite = TRUE)
+devtools::load_all()
 
-# add query default parameters
-qd_to_add <- c("D66_Defaults.xml", "D76_Defaults.xml")
-dbcodes <- purrr::map_chr(strsplit(qd_to_add, "_"), ~.x[1])
+# get database codes
+databases <- dbnamelookup$dbcode
+
+# add label lookup lists
+label_list <- purrr::map(databases, make_label_lookup) %>%
+    setNames(paste0(databases, "labellookup"))
+
+# add query default lookup lists
+qd_to_add <- purrr::map_chr(databases, ~paste0(.x, "_Defaults.xml"))
 query_defaults <- purrr::map(qd_to_add, make_query_list) %>%
-    setNames(paste0(dbcodes, "querydefaults"))
+    setNames(paste0(databases, "querydefaults"))
 
-devtools::use_data(dbnamelookup, label_list, query_defaults, internal = TRUE,
-                   overwrite = TRUE)
+# save as internal data
+devtools::use_data(dbnamelookup, label_list, query_defaults,
+                   internal = TRUE, overwrite = TRUE)
+
+# make codebook vignettes
+purrr::map(databases, make_codebook_vignette)
+devtools::build_vignettes()
+
+# Click Build & Reload to see in Help section (??)
 
