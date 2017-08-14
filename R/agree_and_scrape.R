1ce074196260ffc27e08facfaebfe8b520350a41
jtr13 <jtr13@columbia.edu>
2017-08-14 14:55:54 -0400

    add capability for databases w/o agree button

--- R/agree_and_scrape.R
+++ R/agree_and_scrape.R
@@ -7,18 +7,20 @@ agree_and_scrape <- function(dbname = "natality-current",
 
 
 # determine where the I agree button is located:
-# read_html(url1) %>% html_form()
+# xml2::read_html(url1) %>% rvest::html_form()
 # It's [[3]]
 
 # Submit "I agree" and then get web site contents
 # (See: https://stackoverflow.com/questions/39779004/having-difficulty-navigating-webpages-using-rvest-package)
 
-session <- rvest::html_session(url1)
-form <- rvest::html_form(session)[[3]]
-
-webdata <- rvest::submit_form(session, form, submit = submit) %>%
-    xml2::read_html(url2)
-
+    if (!is.na(submit)) {
+        session <- rvest::html_session(url1)
+        form <- rvest::html_form(session)[[3]]
+        webdata <- rvest::submit_form(session, form, submit = submit) %>%
+            xml2::read_html(url2)
+    } else {
+        webdata <- xml2::read_html(url1)
+    }
 return(webdata)
 }
 
